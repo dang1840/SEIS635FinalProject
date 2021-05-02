784c448ee6b2f0f29868ad59f127db1ec550b5b1
dang1840 <71092639+dang1840@users.noreply.github.com>
2021-05-01 19:55:21 -0500

    Update TicTacToe.java

--- src/main/java/com/seis635/group/tictactoe/TicTacToe.java
+++ src/main/java/com/seis635/group/tictactoe/TicTacToe.java
@@ -16,14 +16,12 @@ public class TicTacToe {
      */
     public static void main(String[] args) {
 
-        String filePath = "bensound-happyrock.wav";
-        BackgroundMusic background = new BackgroundMusic();
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
                     UserLogin frame = new UserLogin();
                     frame.setVisible(true);
-                    //background.playMusic(filePath);
+                    
 
                 } catch (Exception e) {
                     e.printStackTrace();
